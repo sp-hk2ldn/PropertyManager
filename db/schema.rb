7794e669e5fa13bd681afba13ab1dff224f29542
Stephen <stephen.parker@timehascome.org>
2014-11-19 11:52:17 +0800

    on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141106065456) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contractors", force: true do |t|
     t.string   "company_name"
     t.string   "industry_type"
