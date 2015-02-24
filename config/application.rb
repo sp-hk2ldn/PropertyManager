ecb3cbb2bab1b0e769f1cbd187444660b1842e61
Stephen <stephen.parker@timehascome.org>
2015-02-24 19:11:11 +0000

    rolled back heroku to v1

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,11 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.serve_static_assets = true
 module PropertyManager
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
