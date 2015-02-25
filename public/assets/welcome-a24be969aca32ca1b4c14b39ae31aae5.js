871a7f75451514b1ce7e1bb9da79207980031794
Stephen <stephen.parker@timehascome.org>
2015-02-25 10:21:25 +0000

    precompiled

---
+++ public/assets/welcome-a24be969aca32ca1b4c14b39ae31aae5.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  
+  function getData() {
+    return $.ajax({
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
+      url: '/get_data.json',
+      dataType: 'json',
+    });
+  }
+
+  var promise = getData();
+
+  promise.success(function (data) {
+    // var div1=d3.select(document.getElementById('radial-1'));
+
+    start();
+
+
+    function start() {
+      var rp1 = radialProgress(document.getElementById('radial-1'))
+              .label("Outstanding Work Orders")
+              .diameter(150)
+              .value(data[0])
+              .render();
+      var rp2 = radialProgress(document.getElementById('radial-2'))
+              .label("Properties Managed")
+              .diameter(150)
+              .value(data[1])
+              .render();
+    }
+  });
+})
+;
